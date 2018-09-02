26c763128014a646eddba48512ddc60dfe1fd3c9
Rommel Rico <me@rommelrico.com>
2018-09-01 20:46:19 -0700

    Adding a list of random strings to the list view

--- app/src/main/java/com/rommelrico/weatherapp/ForecastActivity.kt
+++ app/src/main/java/com/rommelrico/weatherapp/ForecastActivity.kt
@@ -2,11 +2,20 @@ package com.rommelrico.weatherapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class ForecastActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast)
+
+        var listView = findViewById<ListView>(R.id.forecastListView)!!
+
+        var randomThings = listOf("Hello", "How are you?", "I like cheese")
+
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, randomThings)
+        listView.adapter = adapter
     }
 }
