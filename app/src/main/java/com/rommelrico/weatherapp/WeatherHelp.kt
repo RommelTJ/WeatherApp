c029ed81ca3797f6dd611715feb119ad86806c0b
Rommel Rico <me@rommelrico.com>
2018-09-03 08:48:06 -0700

    Adding get annotation with path to resource in MyJSON.com

--- app/src/main/java/com/rommelrico/weatherapp/WeatherHelp.kt
+++ app/src/main/java/com/rommelrico/weatherapp/WeatherHelp.kt
@@ -1,8 +1,11 @@
 package com.rommelrico.weatherapp
 
 import retrofit2.Call
+import retrofit2.http.GET
 
 interface WeatherAPI {
+
+    @GET("bins/r8vzg")
     fun getForecast(): Call<List<Forecast>>
 }
 
