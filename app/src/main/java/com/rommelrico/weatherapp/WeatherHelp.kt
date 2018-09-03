8cbe773c3532057e0b8de82c54dc585669335058
Rommel Rico <me@rommelrico.com>
2018-09-03 08:54:32 -0700

    WeatherRetriever utility class

--- app/src/main/java/com/rommelrico/weatherapp/WeatherHelp.kt
+++ app/src/main/java/com/rommelrico/weatherapp/WeatherHelp.kt
@@ -1,6 +1,8 @@
 package com.rommelrico.weatherapp
 
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface WeatherAPI {
@@ -10,3 +12,15 @@ interface WeatherAPI {
 }
 
 class Forecast(val high: String, val low: String)
+
+class WeatherRetriever {
+    val service: WeatherAPI
+
+    init {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.myjson.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        this.service = retrofit.create(WeatherAPI::class.java)
+    }
+}
